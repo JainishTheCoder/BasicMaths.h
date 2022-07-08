c5fa53397da3fe430baeb0649a5a7784786e050e
Jainish Prajapati <jainishprajapati98@gmail.com>
2022-07-08 17:38:49 +0530

    Add files via upload

---
+++ basicmaths.h
@@ -0,0 +1,59 @@
+inline int product(int x,int y)
+{
+    return x*y;
+}
+inline int product(int x,int y,int z)
+{
+    return x*y*z;
+}
+inline int product(double x,double y)
+{
+    return x*y;
+}
+inline int product(double x,double y,double z)
+{
+    return x*y*z;
+}
+
+inline int sum(int x,int y)
+{
+    return x+y;
+}
+inline int sum(int x,int y,int z)
+{
+    return x+y+z;
+}
+inline int sum(double x,double y)
+{
+    return x+y;
+}
+inline int sum(double x,double y,double z)
+{
+    return x+y+z;
+}
+
+inline int subtraction(int x,int y)
+{
+    return x-y;
+}
+inline int subtraction(int x,int y,int z)
+{
+    return x-y-z;
+}
+inline int subtraction(double x,double y)
+{
+    return x-y;
+}
+inline int subtraction(double x,float y,double z)
+{
+    return x-y-z;
+}
+
+inline int division(int x,int y)
+{
+    return x/y;
+}
+inline int division(double x,double y)
+{
+    return x/y;
+}
